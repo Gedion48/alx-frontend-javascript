702a912aebb1d4e0d702892ccc2479eabb5ef1d5
Gedion Fetene <gedionfetene77@gmail.com>
2025-07-13 22:40:51 +0300

    Update main.ts with required exports and logs

--- frontend-javascript/0x04-TypeScript/task_4/js/main.ts
+++ frontend-javascript/0x04-TypeScript/task_4/js/main.ts
@@ -1,14 +1,13 @@
-/// <reference path="./subjects/Teacher.ts" />
-/// <reference path="./subjects/Subject.ts" />
-/// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/Java.ts" />
-/// <reference path="./subjects/React.ts" />
+import { Cpp } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
+import { Teacher } from './subjects/Teacher';
 
-const cpp = new Subjects.Cpp();
-const java = new Subjects.Java();
-const react = new Subjects.React();
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
-const cTeacher: Subjects.Teacher = {
+export const cTeacher: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
   experienceTeachingC: 10,
@@ -28,4 +27,3 @@ console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
